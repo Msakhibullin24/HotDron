73858ce2c3d87c28d42fac35e37f5fef13541f31
ropranam <ysatts@gmail.com>
2025-06-27 17:12:55 +0300

    Not working fly v1

--- drone/lib.py
+++ drone/lib.py
@@ -1,10 +1,12 @@
 import math
 import logging
+import sys
 
 import rospy
 from clover import srv
 from std_srvs.srv import Trigger
-
+from mavros_msgs.srv import CommandBool, SetMode
+from geometry_msgs.msg import PoseStamped
 
 class HotDrone:
     def __init__(self) -> None:
@@ -12,7 +14,14 @@ class HotDrone:
         self.navigate = rospy.ServiceProxy('navigate', srv.Navigate)
         self.get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
         self.set_led = rospy.ServiceProxy('led/set_effect', srv.SetLEDEffect)
+
+        self.force_arm = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
+        
         self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        self.logger.addHandler(handler)
 
     def navigate_wait(
         self,
@@ -28,6 +37,11 @@ class HotDrone:
         self.logger.info(f"Navigating to x={x:.2f} y={y:.2f} z={z:.2f} in {frame_id}")
         self.navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
 
+        telem = self.get_telemetry(frame_id="body")
+        self.logger.info(telem)
+        if not telem.armed:
+            raise RuntimeError("Arming failed!")
+
         while True:
             telem = self.get_telemetry(frame_id="navigate_target")
             if math.sqrt(telem.x**2 + telem.y**2 + telem.z**2) < tolerance:
@@ -40,15 +54,16 @@ class HotDrone:
         self.logger.info(f"Taking off to z={z:.2f}")
         self.set_led(effect='blink', r=255, g=255, b=255)
 
-        self.navigate_wait(z=z, speed=0.5, frame_id="body", auto_arm=True)
+        self.force_arm(True)
+        self.navigate_wait(z=z, speed=0.5, frame_id="aruco", auto_arm=True)
 
-        telem = self.get_telemetry(frame_id="body")
+        telem = self.get_telemetry(frame_id="ned")
         if not telem.armed:
             raise RuntimeError("Arming failed!")
         self.set_led(effect='blink', r=255, g=165, b=0)
 
-        self.wait(delay)
         self.navigate(x=telem.x, y=telem.y, z=z, yaw=float("nan"), speed=0.5, frame_id="aruco_map")
+        self.wait(delay)
 
         self.logger.info("Takeoff done")
         self.set_led(r=0, g=255, b=0)
@@ -73,13 +88,33 @@ class HotDrone:
         
     def run(self) -> None:
         z = 1.1
-        self.takeoff(z=z, delay=0.5)
-        self.wait(10)
 
-        self.navigate_wait(x=0, y=0, z=z, speed=0.5, frame_id="aruco_81", tolerance=0.2)
+
+        # self.takeoff(z=z, delay=0.5)
+        while True:
+            telem = self.get_telemetry(frame_id="body")
+            self.logger.info(telem)
+            self.wait(0.5)
+
+        # self.navigate_wait(x=0, y=0, z=z, speed=0.5, frame_id="aruco_81", tolerance=0.2)
         # self.navigate_wait(x=0.5625, y=0.5625, z=z, speed=0.5, frame_id="aruco_map", tolerance=0.2)
-        self.wait(10)
+        # self.wait(10)
 
-        self.land()
-        self.wait(10)
+        # self.land()
+
+    def send_fake_pos(self):
+        pub = rospy.Publisher('/mavros/vision_pose/pose', PoseStamped, queue_size=1)
+
+        msg = PoseStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = "map"
+        msg.pose.position.x = -0.9737948572061019
+        msg.pose.position.y = 1.3070810124006333
+        msg.pose.position.z = -1.3941157568261844
+        msg.pose.orientation.x = -0.007933059759995434
+        msg.pose.orientation.y = -0.009343381468109018
+        msg.pose.orientation.z = -0.6053151200902469
+        msg.pose.orientation.w = 0.7958915586785147
+        pub.publish(msg)
+        return pub
 
